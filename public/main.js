b804d526e32ca199928994fd69faf545c61451cf
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-07-20 17:15:10 +0530

    modals implemented + added styling

--- public/main.js
+++ public/main.js
@@ -4,16 +4,85 @@ var url;
 function getDevice(deviceName) {
     var currDevice = {};
     switch (deviceName) {
-        case "small":
-            currDevice.name = "small";
-            currDevice.height = 360;
+        case "Laptop HD":
+            currDevice.name = "Laptop HD";
+            currDevice.height = 1080;
+            currDevice.width = 1920;
+            break;
+
+        
+        case "Laptop large":
+            currDevice.name = "Laptop large";
+            currDevice.height = 768;
+            currDevice.width = 1366;
+            break;
+
+        
+        case "Laptop wxga–wide":
+            currDevice.name = "Laptop wxga–wide";
+            currDevice.height = 800;
+            currDevice.width = 1280;
+            break;
+
+        
+        case "iPad Pro":
+            currDevice.name = "iPad Pro";
+            currDevice.height = 1024;
+            currDevice.width = 1366;
+            break;
+
+        case "iPad Mini":
+            currDevice.name = "iPad Mini";
+            currDevice.height = 768;
+            currDevice.width = 1024;
+            break;
+        
+        case "iPhone X":
+            currDevice.name = "iPhone X";
+            currDevice.height = 812;
+            currDevice.width = 375;
+            break;
+
+        case "iPhone 8 Plus":
+            currDevice.name = "iPhone (8/7/6/6S) Plus";
+            currDevice.height = 736;
+            currDevice.width = 414;
+            break;
+
+        case "iPhone 8":
+            currDevice.name = "iPhone 8/7/6/6S";
+            currDevice.height = 667;
+            currDevice.width = 375;
+            break;
+
+        case "iPhone 5":
+            currDevice.name = "iPhone 5";
+            currDevice.height = 558;
+            currDevice.width = 320;
+            break;
+
+        case "Nexus 6P":
+            currDevice.name = "Nexus 6P";
+            currDevice.height = 731;
+            currDevice.width = 411;
+            break;
+
+        case "Samsung Galaxy Note 5":
+            currDevice.name = "Samsung Galaxy Note 5";
+            currDevice.height = 853;
             currDevice.width = 480;
             break;
 
-        case "medium":
-            currDevice.name = "medium";
-            currDevice.height = 480;
-            currDevice.width = 640;
+        case "Samsung Galaxy S9":
+            currDevice.name = "Samsung Galaxy S9";
+            currDevice.height = 740;
+            currDevice.width = 360;
+            break;
+
+        case "Samsung Galaxy S7":
+            currDevice.name = "Samsung Galaxy S7";
+            currDevice.height = 640;
+            currDevice.width = 360;
             break;
 
         default:
