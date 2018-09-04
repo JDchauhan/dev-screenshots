7e06fdc41129cfeb3f97d67d0fc4851ac22be0f3
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-09-04 11:52:24 +0530

    styling

--- public/js/payment.js
+++ public/js/payment.js
@@ -38,108 +38,108 @@ $(function () {
 
             }).fail(function (xhr, status, error) {
 
-                setCookie("token", "", -1);
-                window.location.href = "/login";
-            });
+            setCookie("token", "", -1);
+            window.location.href = "/login";
+        });
     }
 
     //open payment dialog
     $(document).on('click', '#payment', function () {
         payumoney();
     });
-});
 
 
-//get hash for payment
-function payumoney() {
-    // Data to be Sent to API to generate hash.
-    let data = {
-        'txnid': RequestData.txnid,
-        'email': RequestData.email,
-        'amount': RequestData.amount,
-        'productinfo': RequestData.productinfo,
-        'firstname': RequestData.firstname
+    //get hash for payment
+    function payumoney() {
+        // Data to be Sent to API to generate hash.
+        let data = {
+            'txnid': RequestData.txnid,
+            'email': RequestData.email,
+            'amount': RequestData.amount,
+            'productinfo': RequestData.productinfo,
+            'firstname': RequestData.firstname
+        }
+
+        // API call to get the Hash value
+        fetch('http://localhost:3000/payment/payumoney', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(function (a) {
+                return a.json();
+            })
+            .then(function (json) {
+                RequestData.hash = json['hash']
+                //  With the hash value in response, we are ready to launch the bolt overlay.
+                //Function to launch BOLT   
+                console.log(RequestData);
+                bolt.launch(RequestData, {
+                    responseHandler: function (response) {
+                        fetch('http://localhost:3000/payment/payumoney/response', {
+                                method: 'POST',
+                                headers: {
+                                    'Accept': 'application/json',
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify(response.response)
+                            })
+                            .then(function (a) {
+                                return a.json();
+                            })
+                            .then(function (json) {
+                                console.log(json);
+                            });
+                    },
+                    catchException: function (BOLT) {
+                        console.log(BOLT);
+                    }
+                });
+            });
     }
 
-    // API call to get the Hash value
-    fetch('http://localhost:3000/payment/payumoney', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-        .then(function (a) {
-            return a.json();
-        })
-        .then(function (json) {
-            RequestData.hash = json['hash']
-            //  With the hash value in response, we are ready to launch the bolt overlay.
-            //Function to launch BOLT   
-            console.log(RequestData);
-            bolt.launch(RequestData, {
-                responseHandler: function (response) {
-                    fetch('http://localhost:3000/payment/payumoney/response', {
-                        method: 'POST',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(response.response)
-                    })
-                        .then(function (a) {
-                            return a.json();
-                        })
-                        .then(function (json) {
-                            console.log(json);
-                        });
+
+
+    //stripe
+
+    var handler = StripeCheckout.configure({
+        key: 'pk_test_a09RA0CrRjZQFvHO1gcQ1way',
+        image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+        locale: 'auto',
+        token: function (token) {
+            $.ajax({
+                url: "http://localhost:3000/payment/stripe",
+                type: 'POST',
+                data: JSON.stringify(token),
+                contentType: 'application/json',
+                success: function (result) {
+
+                    console.log("success");
                 },
-                catchException: function (BOLT) {
-                    console.log(BOLT);
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log("error");
                 }
             });
-        });
-}
-
-
-
-//stripe
-
-var handler = StripeCheckout.configure({
-    key: 'pk_test_a09RA0CrRjZQFvHO1gcQ1way',
-    image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-    locale: 'auto',
-    token: function (token) {
-        $.ajax({
-            url: "http://localhost:3000/payment/stripe",
-            type: 'POST',
-            data: JSON.stringify(token),
-            contentType: 'application/json',
-            success: function (result) {
-
-                console.log("success");
-            },
-            error: function (xhr, textStatus, errorThrown) {
-                console.log("error");
-            }
-        });
-    }
-});
-
-document.getElementById('customButton').addEventListener('click', function (e) {
-    // Open Checkout with further options:
-    handler.open({
-        name: 'Hexerve',
-        description: 'Screenshot taker tool',
-        zipCode: true,
-        amount: 599,
-        email: email
+        }
     });
-    e.preventDefault();
-});
 
-// Close Checkout on page navigation:
-window.addEventListener('popstate', function () {
-    handler.close();
+    document.getElementById('customButton').addEventListener('click', function (e) {
+        // Open Checkout with further options:
+        handler.open({
+            name: 'Hexerve',
+            description: 'Screenshot taker tool',
+            zipCode: true,
+            amount: 599,
+            email: email
+        });
+        e.preventDefault();
+    });
+
+    // Close Checkout on page navigation:
+    window.addEventListener('popstate', function () {
+        handler.close();
+    });
 });
