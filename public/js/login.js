65d8d54f6cbd51b46801f70ec3ae6b35e82557ab
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-09-07 16:37:14 +0530

    logged out implemented

--- public/js/login.js
+++ public/js/login.js
@@ -101,4 +101,13 @@ $(function () {
         );
     }
 
+    if (window.location.search.substr(1).split("=")[1] === "logout") {
+        $('#login-msg').append(
+            '<div class="alert alert-success alert-dismissible fade show">' +
+            '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+            '<strong>Congratulation! </strong> You have been successfully Logged out.' +
+            '</div>'
+        );
+    }
+
 });
